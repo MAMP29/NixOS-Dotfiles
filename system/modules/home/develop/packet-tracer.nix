0a2ab474d7e8aa7e3281a0a1e8a23d1b66360d02
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2025-08-30 15:20:50 -0500

    Indicaciones de packet tracer añadidas

--- system/modules/home/develop/packet-tracer.nix
+++ system/modules/home/develop/packet-tracer.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  # Necesita el .deb original para funcionar, descargalo y renombralo a: CiscoPacketTracer822_amd64_signed.deb, luego se ejecuta el comando:
+  # nix-store --add-fixed sha256 CiscoPacketTracer822_Ubuntu_64bit.deb
   home.packages = with pkgs; [
     ciscoPacketTracer8
   ];
