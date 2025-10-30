8d73251ecd89fcc2b0d1bb8af51429fa0410887e
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2025-10-30 09:29:10 -0500

    Estructura de la carpeta wlgout corregida

--- system/modules/home/wlogout/wlogout.nix
+++ system/modules/home/wlogout/wlogout.nix
@@ -5,40 +5,40 @@
     enable = true;
     layout = [
       {
-          "label" : "lock",
-          "action" : "loginctl lock-session",
-          "text" : "Lock",
-          "keybind" : "l"
+          "label" = "lock";
+          "action" = "loginctl lock-session";
+          "text" = "Lock";
+          "keybind" = "l";
       }
       {
-          "label" : "reboot",
-          "action" : "systemctl reboot",
-          "text" : "Reboot",
-          "keybind" : "r"
+          "label" = "reboot";
+          "action" = "systemctl reboot";
+          "text" = "Reboot";
+          "keybind" = "r";
       }
       {
-          "label" : "shutdown",
-          "action" : "systemctl poweroff",
-          "text" : "Shutdown",
-          "keybind" : "s"
+          "label" = "shutdown";
+          "action" = "systemctl poweroff";
+          "text" = "Shutdown";
+          "keybind" = "s";
       }
       {
-          "label" : "logout",
-          "action" : "hyprctl dispatch exit || pkill Hyprland || loginctl terminate-user $USER",
-          "text" : "Logout",
-          "keybind" : "e"
+          "label" = "logout";
+          "action" = "hyprctl dispatch exit || pkill Hyprland || loginctl terminate-user $USER";
+          "text" = "Logout";
+          "keybind" = "e";
       }
       {
-          "label" : "suspend",
-          "action" : "systemctl suspend",
-          "text" : "Suspend",
-          "keybind" : "u"
+          "label" = "suspend";
+          "action" = "systemctl suspend";
+          "text" = "Suspend";
+          "keybind" = "u";
       }
       {
-          "label" : "hibernate",
-          "action" : "systemctl hibernate",
-          "text" : "Hibernate",
-          "keybind" : "h"
+          "label" = "hibernate";
+          "action" = "systemctl hibernate";
+          "text" = "Hibernate";
+          "keybind" = "h";
       }
     ];
 
